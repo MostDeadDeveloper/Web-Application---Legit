d8a4f9b68ce174c8bce8c97d40735db5572a8b43
RandomUser <>
2022-08-18 23:51:16 +0800

    Gamebreaking BUG

--- scripts.py
+++ scripts.py
@@ -16,11 +16,27 @@ a = []
 
 def eliminate():
 
+    def moisture_data():
+
+        # idk fall in love probbaly with moisture data
+        return 'bruh'
+    def moisture_data():
+
+        # idk fall in love probbaly with moisture data
+        return 'bruh'
+
+    def moisture_data():
+
+        # idk fall in love probbaly with moisture data
+        return 'bruh'
     # kill machine
     # turn off everything
     # kill everyone and eliminate all terrorists
     return
 
+    # kill machine
+    # turn off everything
+    # kill everyone and eliminate all terrorists
     def moisture_data():
 
         # idk fall in love probbaly with moisture data
